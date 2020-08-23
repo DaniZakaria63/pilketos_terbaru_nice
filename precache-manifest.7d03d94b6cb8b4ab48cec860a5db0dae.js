dcf89a1c2e99ff28ae2c7d5a37f8bf70434c131f
DaniZakaria63 <daniootsutsuki@gmail.com>
2020-08-23 15:08:58 +0700

    Updates

---
+++ precache-manifest.7d03d94b6cb8b4ab48cec860a5db0dae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bf62ba6d40935074ecbba1409ad3189",
+    "url": "/pilketos_terbaru_nice/index.html"
+  },
+  {
+    "revision": "fd9b2bc03b48993bd3e5",
+    "url": "/pilketos_terbaru_nice/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "be96f90d52f8e3bc25eb",
+    "url": "/pilketos_terbaru_nice/static/css/main.fc53a324.chunk.css"
+  },
+  {
+    "revision": "fd9b2bc03b48993bd3e5",
+    "url": "/pilketos_terbaru_nice/static/js/2.eddb07c4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/pilketos_terbaru_nice/static/js/2.eddb07c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be96f90d52f8e3bc25eb",
+    "url": "/pilketos_terbaru_nice/static/js/main.43f5a2fa.chunk.js"
+  },
+  {
+    "revision": "6ce5d9c546481be64a45",
+    "url": "/pilketos_terbaru_nice/static/js/runtime-main.d5fb03a7.js"
+  }
+]);
--- src/api.js
+++
@@ -1,5 +0,0 @@
-import * as others from 'axios';
-
-export default others.create({
-    baseURL: 'http://localhost:3001/api/'
-});

6c78641d3333fa36c94a8f27fe1176e4def13d0a
DaniZakaria63 <daniootsutsuki@gmail.com>
2020-08-25 11:10:57 +0700

    Updates

---
+++ precache-manifest.2f39c00557ea0f2d049a1d21932ea7cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b2418ee431403aacfe8b8379457f884",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8ed7ff24a427b19a0abf",
+    "url": "./static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "2387baef26cc28af5273",
+    "url": "./static/css/main.fc53a324.chunk.css"
+  },
+  {
+    "revision": "8ed7ff24a427b19a0abf",
+    "url": "./static/js/2.29fffa3e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.29fffa3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2387baef26cc28af5273",
+    "url": "./static/js/main.fd370cf7.chunk.js"
+  },
+  {
+    "revision": "a166568c2c9a75d76300",
+    "url": "./static/js/runtime-main.b499d923.js"
+  }
+]);
